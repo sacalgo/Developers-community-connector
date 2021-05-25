a9bb0b9eeb36e12eb7cce00d78a9861d0afa09cb
Sachin <algorithms2019@gmail.com>
2021-05-25 17:44:06 +0530

    request and validation done

--- server.js
+++ server.js
@@ -6,14 +6,17 @@ const app = express();
 //connect Database
 connectDB();
 
+//Init Middleware
+// app.use(bodyParser.json());
+app.use(express.json({ extended: false }));
+
 app.get("/", (req, res) => res.send("Server APi is running ... "));
 
 //routes
-app.use('/api/users', require('./routes/users'));
-app.use('/api/auth', require('./routes/auth'));
-app.use('/api/profile', require('./routes/profile'));
-app.use('/api/posts', require('./routes/posts'));
-
+app.use("/api/users", require("./routes/users"));
+app.use("/api/auth", require("./routes/auth"));
+app.use("/api/profile", require("./routes/profile"));
+app.use("/api/posts", require("./routes/posts"));
 
 const PORT = process.env.PORT || 7000;
 
