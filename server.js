c371ec89cc05c0bdada69311644b0fe8c2a5145e
Sachin <algorithms2019@gmail.com>
2021-05-25 17:12:44 +0530

    added basic routes

--- server.js
+++ server.js
@@ -8,6 +8,13 @@ connectDB();
 
 app.get("/", (req, res) => res.send("Server APi is running ... "));
 
+//routes
+app.use('/api/users', require('./routes/users'));
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/profile', require('./routes/profile'));
+app.use('/api/posts', require('./routes/posts'));
+
+
 const PORT = process.env.PORT || 7000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
