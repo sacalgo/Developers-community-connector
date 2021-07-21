9afc0817b4cc459235ce830b48d81319b8610eab
Sachin <algorithms2019@gmail.com>
2021-07-22 04:53:16 +0530

    added alert component and completed it's working functionality

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,9 +1,12 @@
 /* eslint-disable no-unused-vars */
 import React, { Fragment, useState } from "react";
-import {Link } from 'react-router-dom';
+import { connect } from "react-redux";
+import { Link } from "react-router-dom";
 // import axios from "axios";
+import { setAlert } from "../../actions/alert";
+import PropTypes from "prop-types";
 
-const Register = () => {
+const Register = ({ setAlert }) => {
   const [formData, setFormData] = useState({
     name: "",
     email: "",
@@ -17,7 +20,7 @@ const Register = () => {
   const onSubmit = async (e) => {
     e.preventDefault();
     if (password !== password2) {
-      console.log("Passwords do not match");
+      setAlert("passwords do not match", "danger");
     } else {
       console.log("success");
       //   const newUser = {
@@ -99,5 +102,8 @@ const Register = () => {
     </Fragment>
   );
 };
-
-export default Register;
+Register.propTypes = {
+  setAlert: PropTypes.func.isRequired,
+};
+//props.setAlert
+export default connect(null, { setAlert })(Register);
