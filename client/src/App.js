1b5f505683a5e136a7dbab3e4e04506a9e5e1a0b
Sachin <algorithms2019@gmail.com>
2021-07-23 04:24:03 +0530

    is authenticated user or not? functionality added

--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,43 @@
 /* eslint-disable no-unused-vars */
-import React, { Fragment } from "react";
+import React, { Fragment, useEffect } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Navbar from "./components/layout/Navbar";
 import Landing from "./components/layout/Landing";
 import Register from "./components/auth/Register";
 import Login from "./components/auth/Login";
 import Alert from "./components/layout/Alert";
+import { loadUser } from "./actions/auth";
+import setAuthToken from "./utils/setAuthToken";
+
 import "./App.css";
 //Redux
 import { Provider } from "react-redux";
 import store from "./store";
-const App = () => (
-  <Provider store={store}>
-    <Router>
-      <Navbar />
-      <Route exact path="/" component={Landing} />
-      <section className="container">
-        <Alert />
-        <Switch>
-          <Route exact path="/register" component={Register} />
-          <Route exact path="/login" component={Login} />
-        </Switch>
-      </section>
-    </Router>
-  </Provider>
-);
+
+if (localStorage.token) {
+  setAuthToken(localStorage.token);
+}
+
+const App = () => {
+  useEffect(() => {
+    store.dispatch(loadUser());
+  }, []);
+
+  return (
+    <Provider store={store}>
+      <Router>
+        <Navbar />
+        <Route exact path="/" component={Landing} />
+        <section className="container">
+          <Alert />
+          <Switch>
+            <Route exact path="/register" component={Register} />
+            <Route exact path="/login" component={Login} />
+          </Switch>
+        </section>
+      </Router>
+    </Provider>
+  );
+};
 
 export default App;
---
+++ client/src/utils/setAuthToken.js
@@ -0,0 +1,12 @@
+/* eslint-disable no-unused-vars */
+import axios from "axios";
+
+const setAuthToken = (token) => {
+  if (token) {
+    axios.defaults.headers.common["x-auth-token"] = token;
+  } else {
+    delete axios.defaults.headers.common["x-auth-token"];
+  }
+};
+
+export default setAuthToken;
