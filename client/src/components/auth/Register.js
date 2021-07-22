b90e8898551f6564051f1264219dc6b4af707311
Sachin <algorithms2019@gmail.com>
2021-07-23 04:58:00 +0530

    login user completed at client side

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,13 +1,13 @@
 /* eslint-disable no-unused-vars */
 import React, { Fragment, useState } from "react";
 import { connect } from "react-redux";
-import { Link } from "react-router-dom";
+import { Link, Redirect } from "react-router-dom";
 // import axios from "axios";
 import { setAlert } from "../../actions/alert";
 import { register } from "../../actions/auth";
 import PropTypes from "prop-types";
 
-const Register = ({ setAlert, register }) => {
+const Register = ({ setAlert, register, isAuthenticated }) => {
   const [formData, setFormData] = useState({
     name: "",
     email: "",
@@ -44,6 +44,11 @@ const Register = ({ setAlert, register }) => {
       //   }
     }
   };
+
+  //Redirect after registered
+  if (isAuthenticated) {
+   return <Redirect to="/dashboard" />;
+  }
 
   return (
     <Fragment>
@@ -107,6 +112,12 @@ const Register = ({ setAlert, register }) => {
 Register.propTypes = {
   setAlert: PropTypes.func.isRequired,
   register: PropTypes.func.isRequired,
+  isAuthenticated: PropTypes.bool,
 };
+
+const mapStateToProps = (state) => ({
+  isAuthenticated: state.auth.isAuthenticated,
+});
+
 //props.setAlert
-export default connect(null, { setAlert, register })(Register);
+export default connect(mapStateToProps, { setAlert, register })(Register);
