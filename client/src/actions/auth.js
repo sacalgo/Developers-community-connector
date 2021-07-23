78d0d009c36168e569d8b3f7b8da7706c8fed6f2
Sachin <algorithms2019@gmail.com>
2021-07-23 05:43:48 +0530

    logout functionality completed and it showed into navbar

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -8,6 +8,7 @@ import {
   AUTH_ERROR,
   LOGIN_SUCCESS,
   LOGIN_FAIL,
+  LOGOUT,
 } from "./types";
 import setAuthToken from "../utils/setAuthToken";
 
@@ -84,3 +85,8 @@ export const login = (email, password) => async (dispatch) => {
     });
   }
 };
+
+//Logout /clear profile
+export const logout=()=>dispatch=>{
+  dispatch({type:LOGOUT});
+}
--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -4,9 +4,9 @@ export const REMOVE_ALERT = "REMOVE_ALERT";
 export const REGISTER_SUCCESS = "REGISTER_SUCESS";
 export const REGISTER_FAIL = "REGISTER_FAIL";
 
-
 export const LOGIN_SUCCESS = "LOGIN_SUCESS";
 export const LOGIN_FAIL = "LOGIN_FAIL";
+export const LOGOUT = "LOGOUT";
 
 export const USER_LOADED = "USER_LOADED";
 export const AUTH_ERROR = "AUTH_ERROR";
